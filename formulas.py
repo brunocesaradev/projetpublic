07f00a081fa88b871fdfa3f8828378fb0c020b35
brunocesaradev <126360445+brunocesaradev@users.noreply.github.com>
2023-03-01 20:37:55 -0300

    mudança de codigo

--- formulas.py
+++ formulas.py
@@ -1,18 +1,16 @@
 
 print("Olá, vamos descobrir o comprimento de uma circunfência?")
 raio =  int(input('Digite um número entre 1 e 10 ! > '))   
-PI = 3.141592                                                     # declara uma constante
-compri_circun = 2*raio *PI                                        # realiza a operação
-resultado = round(compri_circun,6)                                # define numero de casas decimais
-print (f"O comprimento da circunferência é >{resultado} ")        # imprime o resultado para o usuario 4
+PI = 3.141592                                                     
+compri_circun = 2*raio *PI                                      
+resultado = round(compri_circun,6)                                
+print (f"O comprimento da circunferência é >{resultado} ")        
 print(''' 
 Muito rápido né? 
 Podemos continuar calculando !
 O que acha de tentarmos descobrir a área de um circulo?''')
-
-
-raio2 = int(input('Para isso, digite um novo valor para o raio >'))     # recebe um novo valor de raio para a 2 operação  
-resultado2 = PI * raio2**2                                               # realiza a operação
-compress_decimal = round(resultado2,6)                                   # define o numero de casas
-print(f"A área do círculo é > {compress_decimal}")                      #imprimi o resultado
+raio2 = int(input('Para isso, digite um novo valor para o raio >'))      
+resultado2 = PI * raio2**2                                              
+compress_decimal = round(resultado2,6)                                   
+print(f"A área do círculo é > {compress_decimal}")                     
 #A = π r²
