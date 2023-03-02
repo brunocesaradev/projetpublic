e81845edebbc3b3fe25409a1a3830c153871d78a
brunocesaradev <126360445+brunocesaradev@users.noreply.github.com>
2023-03-01 21:08:39 -0300

    formula para volume da esfera

---
+++ volumeesf.py
@@ -0,0 +1,6 @@
+print("Vamos calcular o volume da uma esfera")
+raio = int(input('Digite um número para o raio dessa esfera:'))
+PI = 3.141592
+volume = 4/3*PI*raio**3
+resultado = round(volume,6)
+print(f"O volume da esfera é:{resultado}  ")
